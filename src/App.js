ed2b15431cd448b9b89a582b31e9ef147d256b4e
mengbin.li <mengbin.li@szhittech.com>
2020-04-27 13:54:18 +0800

    add dom  router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+function Index() {
+  return <h2>JSPang.com</h2>;
+}
+
+function List() {
+  return <h2>List-Page</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li> <Link to="/">首页</Link></li>
+        <li><Link to="/list/">列表</Link></li>
+      </ul>
+      <Route path="/" exact component={Index} />
+      <Route path="/list/" component={List} />
+    </Router>
   );
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 
 export default App;
